8628077e48a3af68173c755af7b26dc19d21a841
mnyet <janbercalamba10@gmail.com>
2024-01-31 11:00:08 +0800

    Added links to app.

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
             <FontAwesomeIcon className='text-xl my-auto' icon={faGithub} />
             <p className='font-semibold'>Source Code</p>
           </a>
-          <p className='text-center font-light'>mnyessss 2024</p>
+          <a href='https://bear-web-portfolio.vercel.app/' target='_blank' rel='noreferrer' className='text-center font-light'>mnyessss 2024</a>
         </div>
       </div>
     </div>
